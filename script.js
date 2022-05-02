67f5b9ec21bd2d64e7ea707f9e1492f8ba4e6402
gilberto <gsqwow@gmail.com>
2022-05-01 21:21:49 -0600

    Add selection of number 5 functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const output = document.querySelector("#output");
+const numbers = document.getElementsByClassName("numbers");
+const add = document.querySelector("#add");
+
+
+let currentNumber;
+
+const select = function(number){
+    output.textContent += number;
+    currentNumber = output.textContent;
+    return parseInt(currentNumber);
+} 
+
+const sum = function(number, second){
+    
+};
+
+
+five.addEventListener("click", function(){
+    select(five.textContent);
+    console.log(currentNumber);
+});
+
+add.addEventListener("click", function(){
+
+});
