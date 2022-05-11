36e8aa1de93fcc78a6749e8df1b00c5ea565765e
gilberto <gsqwow@gmail.com>
2022-05-11 11:09:14 -0600

    Add singular sum and subtract functionality

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
 const output = document.querySelector("#output");
-const numbers = document.getElementsByClassName("numbers");
-const add = document.querySelector("#add");
 const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const plus = document.querySelector("#plus");
+const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear");
+const subtract = document.querySelector("#minus");
 
-
-let currentNumber;
+let currentNumber, nextNumber;
 let operator;
 let result;
 
-const selectNumber = function(number){
-    output.textContent += number;
-    currentNumber = output.textContent;
-    return parseInt(currentNumber);
-} 
+const add = (a,b) => {
+    result = a + b;
+    output.textContent = result;
+    return result;
+}
 
-const sum = function(number, second){
-    return result = number + second;
-};
+const minus = (a,b) => {
+    if (a < b){
+        result = b - a;
+    } else {
+        result = a - b;
+    }
+    output.textContent = result;
+    return result;
+}
 
+const operate = function(operator, a){
+    b = parseInt(output.textContent);
+
+    if (operator == "add"){
+        add(a, b);
+    } else if (operator == "minus"){
+        minus(a, b);
+    }
+}
+
+const choice = function(operatorChoice){
+    return operator = operatorChoice;
+}
+
+plus.addEventListener("click", function(){
+    choice("add");
+    currentNumber = parseInt(output.textContent);
+    output.textContent = "";
+    return currentNumber;
+})
+
+subtract.addEventListener("click", function(){
+    choice("minus");
+    currentNumber = parseInt(output.textContent);
+    output.textContent = "";
+    return currentNumber;
+})
 
 five.addEventListener("click", function(){
-    selectNumber(five.textContent);
+    output.textContent += five.textContent;
+})
+
+equals.addEventListener("click", function(){
+    operate(operator, currentNumber, nextNumber);
+})
+
+clear.addEventListener("click", function(){
+    output.textContent = "";
+    currentNumber = 0;
+    operator = undefined;
     console.log(currentNumber);
-});
-
-add.addEventListener("click", function(){
-
-});
+})
