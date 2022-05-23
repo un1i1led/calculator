d6e93ffcf7e1043df1ac8e75ba8ebd48b0a8ffd4
gilberto <gsqwow@gmail.com>
2022-05-23 09:46:27 -0600

    Add chain functionality

--- script.js
+++ script.js
@@ -1,95 +1,108 @@
 const output = document.querySelector("#output");
-const plus = document.querySelector("#plus");
-const equals = document.querySelector("#equals");
-const clear = document.querySelector("#clear");
-const subtract = document.querySelector("#minus");
-const numbers = document.querySelectorAll(".btn-number");
 const resultOutput = document.querySelector("#result-output");
+const clear = document.querySelector("#clear");
+const plusMinus = document.querySelector("#plusMinus");
+const plus = document.querySelector("#plus");
+const subtract = document.querySelector("#minus");
+const multiply = document.querySelector("#multiply");
+const divide = document.querySelector("#division");
+const equals = document.querySelector("#equals");
+const numbers = document.querySelectorAll(".btn-number");
+const operators = document.querySelectorAll(".operator");
 
-let currentNumber, holdValue = 0, update = 0;
-let operator;
-let result;
-let arrayNumbers = [];
+let valueHold = [];
+let operator = "";
+let update = 0;
 
-output.textContent = 0;
-
-const add = (a,b) => {
-    result = a + b;
-    resultOutput.textContent = result;
-    arrayNumbers = [result];
-    console.log(arrayNumbers);
-    return result;
-}
-
-const minus = (a,b) => {
-    result = a - b;
-    output.textContent = result;
-    return result;
-}
-
-const operate = function(operator, a){
-    b = parseInt(output.textContent);
-
-    if (operator == "add"){
-        add(a, b);
-    } else if (operator == "minus"){
-        minus(a, b);
-    }
-}
-
-const choice = function(operatorChoice){
-    return operator = operatorChoice;
-}
-
-const display = (results) => output.textContent = result;
-
-const updateValue = (operator, number) => {
+const clearAll = () => {
     output.textContent = "";
-    console.log(arrayNumbers);
-    result = arrayNumbers.reduce((partialSum, a) => partialSum + a, 0);
-    console.log(result);
-    resultOutput.textContent = result;
-    // display(result);
-} 
+    resultOutput.textContent = "";
+    valueHold = [];
+}
 
-plus.addEventListener("click", function(){
-    choice("add");
-    // currentNumber = parseInt(output.textContent);
-    arrayNumbers.push(parseInt(output.textContent));
-    currentNumber = parseInt(output.textContent);
-    updateValue("add", currentNumber);
-    update = 1;
-    return currentNumber;
-})
-
-
-
-subtract.addEventListener("click", function(){
-    choice("minus");
-    currentNumber = parseInt(output.textContent);
-    output.textContent = "";
-    return currentNumber;
-})
-
-numbers.forEach(number =>{
+numbers.forEach(number => {
     number.addEventListener("click", function handleClick(event){
-        output.textContent += number.textContent;
-    });
+        resultOutput.textContent += number.textContent;
+    })
 });
 
-equals.addEventListener("click", function(){
-    operate(operator, currentNumber, holdValue);
-    console.log(operator + currentNumber + holdValue);
-    holdValue = 0;
-    update = 0;
-    output.textContent = "";
+plus.addEventListener("click", function(){
+    if (valueHold.length == 2){
+        valueHold.push(parseInt(resultOutput.textContent));
+        operate(valueHold);
+    }
+
+    valueHold.push(parseInt(resultOutput.textContent));
+    valueHold.push("+");
+    resultOutput.textContent = "";
+    console.log(valueHold);
+});
+
+subtract.addEventListener("click", function(){
+    if (valueHold.length == 2){
+        valueHold.push(parseInt(resultOutput.textContent));
+        operate(valueHold);
+    }
+
+    valueHold.push(parseInt(resultOutput.textContent));
+    valueHold.push("-");
+    resultOutput.textContent = "";
+    console.log(valueHold);
+});
+
+multiply.addEventListener("click", function(){
+    if (valueHold.length == 2){
+        valueHold.push(parseInt(resultOutput.textContent));
+        operate(valueHold);
+    }
+
+    valueHold.push(parseInt(resultOutput.textContent));
+    valueHold.push("x");
+    resultOutput.textContent = "";
+    console.log(valueHold);
+});
+
+divide.addEventListener("click", function(){
+    if (valueHold.length == 2){
+        valueHold.push(parseInt(resultOutput.textContent));
+        operate(valueHold);
+    }
+
+    valueHold.push(parseInt(resultOutput.textContent));
+    valueHold.push("/");
+    resultOutput.textContent = "";
+    console.log(valueHold);
 })
 
-clear.addEventListener("click", function(){
-    output.textContent = 0;
-    arrayNumbers = [];
-    resultOutput.textContent = "";
-    holdValue = 0;
-    operator = undefined;
-    update = 0;
+clear.addEventListener("click", () => {
+    clearAll();
+});
+
+const operate = (hold) => {
+    console.log(hold[1]);
+    if (hold[1] == "+"){
+        result = hold[0] + hold[2];
+    } else if (hold[1] == "-"){
+        result = hold[0] - hold[2];
+    } else if (hold[1] == "x"){
+        result = hold[0] * hold[2];
+    } else if (hold[1] == "/"){
+        if (hold[2] == 0){
+            result = 0;
+        } else {
+            hold[0] / hold[2];
+        }
+    }
+
+    resultOutput.textContent = result;
+    valueHold = [];
+    console.log(valueHold);
+}
+
+equals.addEventListener("click", function(){
+    valueHold.push(parseInt(resultOutput.textContent));
+    console.log(valueHold);
+    operate(valueHold);
 })
+
+
